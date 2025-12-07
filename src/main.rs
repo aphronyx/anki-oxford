8b242685191f08899a7bb4bb66c0e43b4052a9ae
Aphronyx <64679454+aphronyx@users.noreply.github.com>
2025-12-07 22:15:53 +0800

    feat: print phonetic and file name

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,31 @@ async fn main() -> Result<()> {
     let cli = Cli::parse();
     let res = reqwest::get(cli.oxford().url()).await?.text().await?;
     let page = Html::parse_document(&res);
-    let audio_url = page
-        .select(&Selector::from_static("div.phons_br > div.sound"))
+    let pronunciation = page
+        .select(&Selector::from_static("div.phons_br"))
+        .next()
+        .ok_or_else(|| anyhow!("no British pronunciation"))?;
+    let audio_url = pronunciation
+        .select(&Selector::from_static("div.sound"))
         .next()
         .ok_or_else(|| anyhow!("no audio"))?
         .attr("data-src-mp3")
         .ok_or_else(|| anyhow!("no mp3 audio"))?;
     let audio = reqwest::get(audio_url).await?.bytes().await?;
+    let file_name = format!("{}.mp3", cli.oxford().id());
     #[expect(clippy::unwrap_used, reason = "downloads folder exists")]
-    fs::write(
-        download_dir()
-            .unwrap()
-            .join(cli.oxford().id())
-            .with_extension("mp3"),
-        audio,
-    )
-    .await?;
+    fs::write(download_dir().unwrap().join(&file_name), audio).await?;
+
+    for text in pronunciation
+        .select(&Selector::from_static("span.phon"))
+        .next()
+        .ok_or_else(|| anyhow!("no phonetic"))?
+        .text()
+    {
+        print!("{text}");
+    }
+
+    print!("[sound:{file_name}]");
 
     Ok(())
 }
